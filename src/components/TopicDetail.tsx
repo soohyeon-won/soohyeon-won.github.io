9a0f219313e5d39e80aab825a8567c2b79545f83
soohyeon.won <ppkong22@hanmail.net>
2024-10-27 18:28:49 +0900

    setting react-syntax-highlighter

--- src/components/TopicDetail.tsx
+++ src/components/TopicDetail.tsx
@@ -1,6 +1,9 @@
 // src/TopicDetail.tsx
 import React from 'react';
 import '../styles/TopicDetail.css';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism'; // 원하는 테마로 변경 가능
+import 'prismjs/themes/prism-tomorrow.css'; 
 
 // 섹션의 타입 정의
 interface Section {
@@ -23,7 +26,9 @@ const TopicDetail: React.FC<TopicDetailProps> = ({ title, sections }) => {
         <section className="feature-section" key={index}>
           <h2>{section.title}</h2>
           <p>{section.content}</p>
-          <pre><code>{section.code}</code></pre>
+          <SyntaxHighlighter language="typescript" style={solarizedlight}>
+            {section.code}
+          </SyntaxHighlighter>
         </section>
       ))}
     </div>
