cb758fe9cb07b90cb7ae09ebfb42ed29af138cbe
Soohyeon won <30689094+soohyeon-won@users.noreply.github.com>
2024-11-05 23:19:46 +0900

    tab 추가 (#9)
    * tab 추가
    
    * 하단에서 탭바 변경 시 애니메이션
    
    * 라우터 이동 안되는 버그 수정
    
    ---------
    
    Co-authored-by: soohyeon.won <ppkong22@hanmail.net>

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
-// src/App.tsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, useNavigate, Link } from "react-router-dom";
 import TopicsList from "./components/TopicsList";
 import TopicPage from "./components/TopicPage";
+import ComponentsLifecycle from './components/practice/ComponentsLifecycle';
+import './styles/App.css';
 
 const App: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<number>(1);
+
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <div className="app-container">
+      {/* 상단 탭 네비게이션 바 */}
+      <div className="top-tabs">
+        <Link to="/topics">
+          <button className={activeTab === 1 ? 'top-tab active' : 'top-tab'} onClick={() => handleTabClick(1)}>
+            Study
+          </button>
+        </Link>
+        <Link to="/components-lifecycle">
+          <button className={activeTab === 2 ? 'top-tab active' : 'top-tab'} onClick={() => handleTabClick(2)}>
+            Practice
+          </button>
+        </Link>
+      </div>
+      <div className="tab-indicator" style={{ transform: `translateX(${(activeTab - 1) * 100}%)` }}></div>
+      
+      {/* 라우터 설정 */}
+      <Routes>
+        <Route path="/topics" element={<TopicsList />} />
+        <Route path="/topic/:id" element={<TopicPage />} />
+        <Route path="/components-lifecycle" element={<ComponentsLifecycle />} />
+      </Routes>
+    </div>
+  );
+};
+
+const RootApp: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<TopicsList />} />
-        <Route path="/topic/:id" element={<TopicPage />} />
-      </Routes>
+      <App />
     </Router>
   );
 };
 
-export default App;
+export default RootApp;
