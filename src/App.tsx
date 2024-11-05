43458bb674a43eccaf3f7099b7153bf932409536
soohyeon.won <ppkong22@hanmail.net>
2024-11-05 23:19:30 +0900

    라우터 이동 안되는 버그 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, useNavigate, Link } from "react-router-dom";
 import TopicsList from "./components/TopicsList";
 import TopicPage from "./components/TopicPage";
 import ComponentsLifecycle from './components/practice/ComponentsLifecycle';
@@ -8,15 +7,6 @@ import './styles/App.css';
 
 const App: React.FC = () => {
   const [activeTab, setActiveTab] = useState<number>(1);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (activeTab === 1) {
-      navigate("/topics");
-    } else if (activeTab === 2) {
-      navigate("/components-lifecycle");
-    }
-  }, [activeTab, navigate]);
 
   const handleTabClick = (index: number) => {
     setActiveTab(index);
@@ -26,12 +16,19 @@ const App: React.FC = () => {
     <div className="app-container">
       {/* 상단 탭 네비게이션 바 */}
       <div className="top-tabs">
-        <button className={activeTab === 1 ? 'top-tab active' : 'top-tab'} onClick={() => handleTabClick(1)}>Study</button>
-        <button className={activeTab === 2 ? 'top-tab active' : 'top-tab'} onClick={() => handleTabClick(2)}>Practice</button>
+        <Link to="/topics">
+          <button className={activeTab === 1 ? 'top-tab active' : 'top-tab'} onClick={() => handleTabClick(1)}>
+            Study
+          </button>
+        </Link>
+        <Link to="/components-lifecycle">
+          <button className={activeTab === 2 ? 'top-tab active' : 'top-tab'} onClick={() => handleTabClick(2)}>
+            Practice
+          </button>
+        </Link>
       </div>
       <div className="tab-indicator" style={{ transform: `translateX(${(activeTab - 1) * 100}%)` }}></div>
       
-
       {/* 라우터 설정 */}
       <Routes>
         <Route path="/topics" element={<TopicsList />} />
