7b5eb3a40562472f0484963f50f9adc18dd2eb04
soohyeon.won <main.async@kakao.com>
2024-11-10 15:39:43 +0900

    feat(navigation): Display topic details in right panel when selected from list

--- src/components/TopicsList.tsx
+++ src/components/TopicsList.tsx
@@ -1,12 +1,19 @@
 // src/TopicsList.tsx
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
-import { Link } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import "../styles/TopicsList.css";
 import { topics } from "./data";
 import ComponentsLifecycle from "./practice/ComponentsLifecycle";
+import TopicPage from "./TopicPage";
 
 const TopicsList: React.FC = () => {
+    const [selectedTopicId, setSelectedTopicId] = useState<string | null>(null);
+
+    const handleTopicClick = (id: string) => {
+        setSelectedTopicId(id);
+    };
+    
     return (
         <div className="content-wrapper">
             <div className="left-panel">
@@ -30,17 +37,15 @@ const TopicsList: React.FC = () => {
                     ))}
                 </ul>
             </div>
+            
+            
             <div className="right-panel">
                 <h1 className="title">Practice Area</h1>
-                {/* 리스트 형식으로 컴포넌트 추가 */}
-                <div className="component-item">
-                    <h2>1. Life Cycle</h2>
-                    <ComponentsLifecycle />
-                </div>
-                <div className="component-item">
-                    <div className="component-title">함수형 컴포넌트 vs 클래스 컴포넌트</div>
-                    <div className="component-description">{/* 여기에 다른 컴포넌트 추가 */}</div>
-                </div>
+                {/* 선택한 주제에 따른 컴포넌트 렌더링 */}
+                <Routes>
+                    <Route path="/topic/:id" element={<TopicPage />} />
+                    <Route path="/components-lifecycle" element={<ComponentsLifecycle />} />
+                </Routes>
                 <p>This is where you can develop and view your practice implementations.</p>
             </div>
         </div>
