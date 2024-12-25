6ded40621cf3b04d97a6a20ddb89a9093d98957a
soohyeon-won <main.async@kakao.com>
2024-12-25 14:58:13 +0900

    useState, useEffect study

--- src/components/practice/ComponentsLifecycle.tsx
+++ src/components/practice/ComponentsLifecycle.tsx
@@ -5,9 +5,11 @@ import "prismjs/themes/prism-tomorrow.css";
 import ComponentsBtn from "./ComponentsBtn";
 
 const ComponentsLifecycle = () => {
-    const [lifecycleStage, setLifecycleStage] = useState("Mounted");
+    const [lifecycleStage, setLifecycleStage] = useState("Mounted"); // React의 상태 관리 Hook
 
-    useEffect(() => {
+    // `useEffect` 훅은 컴포넌트가 마운트되거나 업데이트될 때 특정 작업을 수행하도록 합니다. 
+    // 두 번째 인자로 전달되는 의존성 배열을 통해 실행 타이밍을 조정할 수 있습니다
+    useEffect(() => { // useEffect**는 컴포넌트가 렌더링된 이후 실행됩니다.
         console.log("Component Mounted");
         setLifecycleStage("Mounted");
 
@@ -15,16 +17,18 @@ const ComponentsLifecycle = () => {
             console.log("Component Unmounted");
             setLifecycleStage("Unmounted");
         };
-    }, []);
+    }, []); // (빈 배열)을 의존성 배열로 전달했기 때문에 이 코드는 컴포넌트가 **마운트(생성)**되거나 **언마운트(제거)**될 때만 실행
 
     useEffect(() => {
-        if (lifecycleStage !== "Mounted") {
-            console.log("Component Updated");
+        console.log("두번째 lifecycleStage:", lifecycleStage);
+        if (lifecycleStage !== "Mounted" && lifecycleStage !== "Updated") {
+            console.log("두번째 Component Updated");
             setLifecycleStage("Updated");
         }
     }, [lifecycleStage]);
 
-    return (
+    // 언마운트 시: useEffect의 클린업 함수(return으로 정의된 함수)가 호출됩니다.
+    return ( // 클린업 함수
         <div>
             <p>함수형 컴포넌트: {lifecycleStage}</p>
             <p>
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+// Strict Mode
+// 마운트 → 언마운트 → 다시 마운트 과정을 시뮬레이션
 root.render(
   <React.StrictMode>
     <App />
