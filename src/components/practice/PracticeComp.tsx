1da4bd74e7765c16757b8d44a807b822dc676634
soohyeon-won <main.async@kakao.com>
2025-01-26 15:59:14 +0900

    feat: studyComp - useState & input

--- src/components/practice/PracticeComp.tsx
+++ src/components/practice/PracticeComp.tsx
@@ -1,16 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
 import ComponentsLifecycle from "./ComponentsLifecycle"
 import useWindowSize from "./StudyCustomHooks";
+
+import '../../styles/App.css'
 
 // const 함수이름 = (매개변수) => { 함수의 로직 };
 const PracticeComp = () => {  
 
     const { width, height } = useWindowSize();
 
-    return <div>
-        <ComponentsLifecycle></ComponentsLifecycle>
-        <h1>Window Size</h1> {/* 제목 */}
-        <p>Width: {width}px, Height: {height}px</p>
+    const [ isOn, setToggle ] = useState(true);
+    const toggleBtn = () => setToggle(!isOn)
+
+    const [price, setPrice] = useState(0);
+    const incrementPrice = (price: number) => {
+        setPrice((prevValue) => prevValue + price);
+    };
+
+    const [ titleText, setText ] = useState("placeholder");
+    const setTitle = (text: string) => {
+        console.log(text);
+        setText(text);
+    };
+    const [ savedText, setSavedText ] = useState("");
+    const setSavedTitle = () => setSavedText(titleText);
+
+    return <div> 
+        <p>1. useState를 이용한 Toggle버튼</p>
+        <button onClick={toggleBtn}>{isOn ? "ON":"OFF"}</button>
+        <br></br>
+
+        <p>2. 버튼에 있는 값 input으로 옮기기</p>
+        <button onClick={() => incrementPrice(10)}>10+</button>
+        <button onClick={() => incrementPrice(50)}>50+</button>
+        <input value={price}></input>
+
+        <p>3. input Text 저장하기</p>
+        <input onChange={(input) => setTitle(input.target.value)}></input>
+        <p>{titleText}</p>
+        <button onClick={setSavedTitle}>저장</button>
+        <p>{savedText}</p>
     </div>
 }
 
