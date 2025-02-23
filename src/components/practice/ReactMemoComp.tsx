8444c71336e62275f817e8e397923e1279521202
soohyeon-won <main.async@kakao.com>
2025-02-23 14:40:35 +0900

    [study] React.memo 개념 이해하기

---
+++ src/components/practice/ReactMemoComp.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// React.memo로 최적화하기 (with UseMemo, UseCallBack)
+// 리액트에서는 컴포넌트가 랜더링 될 때 하위 컴포넌트들도 랜더링됨
+//
+export const ReactMemoComp = () => {
+    return (
+        <div></div>
+    );
+}
