9d72776011044f2b6de52e7e6587676d864aa1bd
soohyeon-won <main.async@kakao.com>
2025-03-25 13:40:44 +0900

    add recoil <RecoilRoot />

--- src/App.tsx
+++ src/App.tsx
@@ -6,61 +6,66 @@ import "./styles/App.css";
 import PracticeComp from "./components/practice/PracticeComp";
 import Tab from "./components/Tab";
 import { RecoilMainComp } from "./components/Recoil/RecoilMainComp";
+import { RecoilRoot } from "recoil";
 
 const App = () => {
-    const [activeTab, setActiveTab] = useState(1);
+  const [activeTab, setActiveTab] = useState(1);
 
-    let tab1Title = 'study'
-  
-    const handleTabClick = (index: number) => {
-      setActiveTab(index);
-    };
-  
-    // 꼭 필요할 때만 적절하게 사용하는 것이 좋다
-    const tabIndicatorStyle = useMemo(() => ({
-      transform: `translateX(${(activeTab - 1) * 100}%)`
-    }), [activeTab]);
-  
-    return (
-      <div className="app-container">
-        <div className="top-tabs">
-          <Tab 
-            to="/study" 
-            label={tab1Title} 
-            isActive={activeTab === 1} 
-            onClick={() => handleTabClick(1)} 
-          />
-          <Tab 
-            to="/practice" 
-            label="Practice" 
-            isActive={activeTab === 2} 
-            onClick={() => handleTabClick(2)} 
-          />
-          <Tab 
-            to="/recoil" 
-            label="Recoil" 
-            isActive={activeTab === 3} 
-            onClick={() => handleTabClick(3)} 
-          />
-          <div className="tab-indicator" style={tabIndicatorStyle}></div>
-        </div>
-  
-        <Routes>
-          <Route path="/" element={<Navigate to="/study" replace />} />
-          <Route path="/study" element={<TopicsList />} />
-          <Route path="/practice" element={<PracticeComp />} />
-          <Route path="/recoil" element={<RecoilMainComp />} />
-        </Routes>
-      </div>
-    );
+  let tab1Title = 'study'
+
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
   };
 
+  // 꼭 필요할 때만 적절하게 사용하는 것이 좋다
+  const tabIndicatorStyle = useMemo(() => ({
+    transform: `translateX(${(activeTab - 1) * 100}%)`
+  }), [activeTab]);
+
+  return (
+    <div className="app-container">
+      <div className="top-tabs">
+        <Tab
+          to="/study"
+          label={tab1Title}
+          isActive={activeTab === 1}
+          onClick={() => handleTabClick(1)}
+        />
+        <Tab
+          to="/practice"
+          label="Practice"
+          isActive={activeTab === 2}
+          onClick={() => handleTabClick(2)}
+        />
+        <Tab
+          to="/recoil"
+          label="Recoil"
+          isActive={activeTab === 3}
+          onClick={() => handleTabClick(3)}
+        />
+        <div className="tab-indicator" style={tabIndicatorStyle}></div>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/study" replace />} />
+        <Route path="/study" element={<TopicsList />} />
+        <Route path="/practice" element={<PracticeComp />} />
+        <Route path="/recoil" element={
+          <RecoilRoot>
+            <RecoilMainComp />
+          </RecoilRoot>
+        } />
+      </Routes>
+    </div>
+  );
+};
+
 const RootApp = () => {
-    return (
-        <Router>
-            <App />
-        </Router>
-    );
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
 };
 
 export default RootApp;
