f2191e7daff87d107a3daafee6c2289e408726d4
soohyeon-won <main.async@kakao.com>
2025-03-25 14:08:08 +0900

    Recoil - Atom

--- src/components/Recoil/RecoilCounterTutorialComp.tsx
+++ src/components/Recoil/RecoilCounterTutorialComp.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useRecoilState } from "recoil";
-import { countState } from "./RecoilMainComp";
+import { countState } from "./Atom";
 
 function LegacyCounter(props: { count: number, onUp: () => void }) {
 
@@ -39,6 +39,8 @@ function Counter() {
 
     const [count, setCount] = useRecoilState(countState);
 
+    console.log('Counter render')
+
     return (
         <div>
             <p>현재 카운터 값은 {count} 입니다.</p>
@@ -49,7 +51,14 @@ function Counter() {
 
 function DisplayCounter() {
 
+    {/* 
+        전역 상태의 countState를 구독하고 있다는 뜻. 
+        state가 변경 될 때 컴포넌트가 렌더링 됨
+        중간 컴포넌트는 렌더링 되지 않음.
+    */}
     const [count] = useRecoilState(countState);
+
+    console.log('DisplayCounter render')
 
     return (
         <div>
