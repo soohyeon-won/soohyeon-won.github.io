049e272f6f7249581fba869cff5de8388d39af28
soohyeon-won <main.async@kakao.com>
2025-03-28 17:25:55 +0900

    css -  assetPrefix: "/soohyeon-won.github.io/", // 정적 자원 경로 (끝에 / 포함)

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // 정적 파일 내보내기 활성화
-  assetPrefix: '.',
+  basePath: "/soohyeon-won.github.io", // GitHub Pages 경로
+  assetPrefix: "/soohyeon-won.github.io/", // 정적 자원 경로 (끝에 / 포함)
   compiler: {
     styledComponents: true,
   },
