0cda2f9358452fdfcbafabee1807600cf6e9bd54
soohyeon-won <main.async@kakao.com>
2025-03-28 16:50:30 +0900

    css 지원 -  assetPrefix: '.' 설정 후 배포 테스트

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // 정적 파일 내보내기 활성화
-  assetPrefix: '/soohyeon-won.github.io/'
+  assetPrefix: '.'
 };
 
 module.exports = nextConfig;
