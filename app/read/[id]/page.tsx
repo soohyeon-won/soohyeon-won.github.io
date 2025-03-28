5e01a9c83a8684c380ab127af6ed5e353988f6f8
soohyeon-won <main.async@kakao.com>
2025-03-28 16:45:24 +0900

    main 폴더 위치 변경

--- app/read/[id]/page.tsx
+++ app/read/[id]/page.tsx
@@ -8,8 +8,6 @@
 //     }
 // }
 
-import React from "react";
-
 interface Topic {
     id: number;
     title: string;
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export" // 정적 파일 내보내기 활성화
+  output: "export", // 정적 파일 내보내기 활성화
+  assetPrefix: '/soohyeon-won.github.io/'
 };
 
 module.exports = nextConfig;
