7551e3c1b07ba45da504d15cb3cf3902dd2bac2b
soohyeon-won <main.async@kakao.com>
2025-03-28 17:08:50 +0900

    css 테스트 - styledComponents: true

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // 정적 파일 내보내기 활성화
-  assetPrefix: '.'
+  assetPrefix: '.',
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 module.exports = nextConfig;
