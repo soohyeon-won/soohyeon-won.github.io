a451056b347247c7156652a80cda6a5e5fa4b702
soohyeon-won <main.async@kakao.com>
2025-03-28 17:27:23 +0900

    Revert "css -  assetPrefix: "/soohyeon-won.github.io/", // 정적 자원 경로 (끝에 / 포함)"
    This reverts commit 049e272f6f7249581fba869cff5de8388d39af28.

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // 정적 파일 내보내기 활성화
-  basePath: "/soohyeon-won.github.io", // GitHub Pages 경로
-  assetPrefix: "/soohyeon-won.github.io/", // 정적 자원 경로 (끝에 / 포함)
+  assetPrefix: '.',
   compiler: {
     styledComponents: true,
   },
